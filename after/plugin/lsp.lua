64d2c2ab22265df0a33ec6d77f511aa6052ae335
Killian DROULEZ <killian.droulez@rwigo.com>
2024-03-26 09:57:20 +0100

    🐛 undefined global `vim` field

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,16 +1,12 @@
 local lsp_zero = require('lsp-zero')
 
-lsp_zero.on_attach(function(client, bufnr)
-  -- see :help lsp-zero-keybindingsi
-  -- to learn the available actions
-  lsp_zero.default_keymaps({buffer = bufnr})
-end)
-
 lsp_zero.setup_servers({"lua_ls", "rust_analyzer", "tsserver"})
 
 lsp_zero.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
-
+  
+  lsp_zero.default_keymaps({buffer = bufnr})
+  
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
@@ -22,3 +18,5 @@ lsp_zero.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 end)
+
+require('lspconfig').lua_ls.setup(lsp_zero.nvim_lua_ls())
